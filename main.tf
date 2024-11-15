8ff57ba447b6a519621727e53b953d4ca93a589c
ramyanerella22 <ramyanerella68@gmail.com>
2024-11-15 18:23:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-012967cc5a8c9f891"
+ami = "ami-0984f4b9e98be44bf"
 instance_type = "t2.medium"
 key_name = "hiii"
 vpc_security_group_ids = ["sg-0aee0e9b3129cc3fe"]
